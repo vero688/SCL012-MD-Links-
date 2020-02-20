34e9702c0492ae072f25b5a59756bdb4e55dbc30
vero688 <vero.urrutiaa@gmila.coname>
2020-02-20 10:42:13 -0300

    cambio en el codigo

--- index.js
+++ index.js
@@ -1,6 +1,19 @@
 const mdLinks = require('./mdlink.js');
 const path = require('path');
 
+// absorve las rutas por el usuario en consola 
+let routFile = process.argv[2];
+//convertir rutas absoluta  (path resolve)
+
+routFile =path.resolve(routFile);
+
+// normaliza los errores de escritura para leerlo con existo (path.normalize)
+
+routFile =path.normalize(routFile);
+
+
+
+
 
 
 
--- mdlink.js
+++ mdlink.js
@@ -2,6 +2,13 @@ const fs = require('fs');
 const marked = require ('marked');
 const fetch = require('node-fetch');
 
+// const mdFromato = ( path =>{
+//     if (path.slice(-3) == '.md'){
+//         return true;
+//     }else {
+//         return false;
+//     }
+// })
 
 
 //leer archivo .md
@@ -18,5 +25,38 @@ const readMd = (path=>{
 })
 })
 
+//obtener links de archivo 
+
+// const searchLink = (path =>{
+//     return new Promise((resolve,reject)=>{
+//         readMd(path).then(result =>{
+//             let links =[];
+//             const renderer = new marked.Renderer();
+//             renderer.links =function(href, title, text){
+                
+//                 link.push({
+//                     //que encuentre url
+//                     href: href,
+//                     //el texto que esta en el link
+//                     text:text,
+//                     //ruta del archivo donde encuentre el link
+//                     file : path })
+
+//             }
+//             //nos muestra el resultado y devulve un links
+//             marked(result,{renderer:renderer});
+//             resolve(link)
+//             console.log(link)
+
+//          })
+//          .catch(error=>{
+//              reject(error)
+//          })
+//      })
+//     })
+
+// searchLink(process.argv[2])
+// module.exports= {
+//     mdLinks
+// }
         
-  
